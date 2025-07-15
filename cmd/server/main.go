49703068b9167fc0360f735c20bbbb34fcf3b45b
achla24 <itsachla24@gmail.com>
2025-07-15 17:21:33 +0530

    first commit from vs

---
+++ cmd/server/main.go
@@ -0,0 +1,9 @@
+//entry point to run the server
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Starting Redis-Go server...")
+    // We will call the server initialization here in Week 3
+}
